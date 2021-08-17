9b0a61ca2df40c096add693de1bdd1a138efa0ff
Ivar Jönsson <49920136+ivario123@users.noreply.github.com>
2021-08-17 20:24:41 +0000

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,37 @@
+import speedtest
+import time
+from datetime import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+count = 0
+print("Started the speed logger.")
+mins = 5
+Times = []
+Down = []
+Up = []
+while 1:
+    try:
+        print("logging")
+        count+=1
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        f = open('data.txt','a')
+        st = speedtest.Speedtest()
+        d_st = st.download()//10**6
+        u_st = st.upload()//10**6
+        Down.append(d_st)
+        Up.append(u_st)
+        Times.append(count*mins/60)
+        plt.plot(Times,Down)
+        plt.xlabel("Time [5 min interval]")
+        plt.ylabel("Mb/s [Rounded to closest integer]")
+        plt.savefig('speed.png')
+        s = f"{current_time} : Down speed = {d_st} Mb/s, Up speed = {u_st} Mb/s\n"
+        f.write(s)
+        f.close()
+        print(s)
+        if count == 60//mins*24:
+            exit
+    except:
+        i = 0
+    time.sleep(mins*60)
